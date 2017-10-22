b9ccb68938045ca96aa99e1fec791cac457b8a75
Taylar Keast <keast.taylar@gmail.com>
2017-10-22 13:43:14 +1300

    Movement
    Added keyevent listeners for client and server so player can move.

--- web/app.js
+++ web/app.js
@@ -11,41 +11,112 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000); //Listen for requests on port 2000
 
 console.log("INFO - server has been started.");
-
-var io = require('socket.io')(serv, {});
 var nextSocketID = 0;
 var SOCKET_LIST = {};
 
-//Listen for connection events
-io.sockets.on('connection', function(socket) {
-    console.log("INFO - client connected to server.");
+var Entity = function() {
+    var self = {
+        id: "",
+        x: 250,
+        y: 250,
+        spdX: 0,
+        spdY: 0
+    }
 
+    self.update = function() {
+        self.updatePosition();
+    }
+
+    self.updatePosition = function() {
+        self.x += self.spdX;
+        self.y += self.spdY;
+    }
+    return self;
+}
+
+var Player = function(id) {
+    var self = Entity();
+    self.id = id;
+    self.pressingRight = false;
+    self.pressingLeft = false;
+    self.pressingUp = false;
+    self.pressingDown = false;
+    self.maxSpd = 10;
+
+    self.updateSpd = function() {
+        if(self.pressingRight) self.spdX = self.maxSpd;
+        else if(self.pressingLeft) self.spdX = -self.maxSpd;
+        else self.spdX = 0;
+
+        if(self.pressingUp) self.spdY = -self.maxSpd;
+        else if(self.pressingDown) self.spdY = self.maxSpd;
+        else self.spdY = 0;
+    }
+
+    var super_update = self.update;
+    self.update = function() {
+        self.updateSpd();
+        super_update();
+    }
+
+    Player.list[id] = self;
+    return self;
+}
+Player.list = {};
+Player.onConnect = function(socket) {
+    //Create player and add to list
+    var player = Player(socket.id);
+
+    //Listen for input events
+    socket.on('keyPress', function(data) {
+        if(data.inputId === 'left') player.pressingLeft = data.state;
+        if(data.inputId === 'right') player.pressingRight = data.state;
+        if(data.inputId === 'up') player.pressingUp = data.state;
+        if(data.inputId === 'down') player.pressingDown = data.state;
+    });
+}
+
+Player.onDisconnect = function(socket) {
+    delete Player.list[socket.id];
+}
+
+Player.update = function() {
+    //Get data from all connected players
+    var pack = [];
+    for(var i in Player.list) {
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            id: player.id,
+            x: player.x,
+            y: player.y
+        });
+    }
+    return pack;
+}
+
+//Listen for connection events
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket) {
+    //Add socket to list
     socket.id = nextSocketID++;
-    socket.x = 0;
-    socket.y = 0;
     SOCKET_LIST[socket.id] = socket;
+    console.log("INFO - client " + socket.id + " connected to the server.");
+
+    Player.onConnect(socket);
 
     //Remove client if disconnected (client sends disconnect automatically)
     socket.on('disconnect', function() {
+        console.log("INFO - client " + socket.id + " disconnected from the server.");
         delete SOCKET_LIST[socket.id];
-    })
+        Player.onDisconnect(socket);
+    });
 });
 
 setInterval(function() {
-    //Get data from all connected clients
-    var pack = [];
-    for(var i in SOCKET_LIST) {
-        var socket = SOCKET_LIST[i];
-        socket.x++;
-        socket.y++;
-        pack.push({
-            id: socket.id,
-            x: socket.x,
-            y: socket.y
-        });
-    }
+    var pack = Player.update();
 
-    //Send client data to all clients
+    //Send player data to all clients
     for(var i in SOCKET_LIST) {
         var socket = SOCKET_LIST[i];
         socket.emit('newPosition', pack);
