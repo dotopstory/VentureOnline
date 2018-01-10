9af029357f59a081decdee20a30d5527c64c2aca
Taylar Keast <keast.taylar@gmail.com>
2018-01-10 19:52:47 +1300

    Added home screen animation

---
+++ client/js/events/events_home.js
@@ -0,0 +1,15 @@
+$(window).on('load', function () {
+    setTimeout(function() { slideDown($('#col1')); }, 10);
+    setTimeout(function() { slideDown($('#col2')); }, 400);
+    setTimeout(function() { slideDown($('#col3')); }, 800);
+    setTimeout(function() { slideDown($('.home-slider')); }, 1200);
+});
+
+function slideDown(element) {
+    element.css({'top': '-200vh'});
+    element.show().animate({top: "0"}, 1000);
+}
+
+function slideUp(element) {
+    element.hide().animate({top: "-200vh"}, 1000);
+}
